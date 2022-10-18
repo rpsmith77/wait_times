46013accfd03684c681fe428778a49e8413cb2a3
Ryan Smith <rpsmith77@gmail.com>
2022-10-17 21:37:34 -0400

    add MUI Icons

--- src/components/WaitTimes.js
+++ src/components/WaitTimes.js
@@ -1,7 +1,20 @@
 import {useEffect, useState} from "react";
 import './WaitTimes.css'
 
-import {Table, TableBody, TableCell, TableContainer, TableRow, Paper, Chip, CircularProgress, Box} from '@mui/material';
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableRow,
+    Paper,
+    Chip,
+    CircularProgress,
+    Box,
+} from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+import EventAvailableIcon from '@mui/icons-material/EventAvailable';
 
 import Park from './disney/parks/park';
 
@@ -20,26 +33,50 @@ function WaitTimes(props) {
             });
     }, [props.id]);
 
+    function displayIconInChip(icon, text, color = 'default') {
+        return <Chip color={color} label={<div style={{
+            display: 'flex',
+            alignItems: 'center',
+            flexWrap: 'wrap',
+            justifyContent: "center",
+        }}>
+            {icon}
+            <span>{text}</span>
+        </div>}/>
+    }
+
     function displayWaitInfo(attraction) {
         let waitInfo = attraction.status;
         if (!(attraction.status === 'OPERATING')) {
-            return waitInfo;
+            if (waitInfo === 'CLOSED') {
+                return displayIconInChip(<CloseIcon/>, 'Closed');
+            }
+            if (waitInfo === 'DOWN') {
+                return displayIconInChip(<ArrowDownwardIcon/>, 'Down', 'error');
+            }
         } else if (attraction.queue.STANDBY) {
             if (attraction.queue.STANDBY.waitTime != null) {
                 waitInfo = attraction.queue.STANDBY.waitTime;
             }
         } else if (attraction.queue.BOARDING_GROUP) {
-            waitInfo = attraction.queue.BOARDING_GROUP.currentGroupStart + '-' + attraction.queue.BOARDING_GROUP.currentGroupEnd;
+            if (attraction.queue.BOARDING_GROUP.currentGroupStart || attraction.queue.BOARDING_GROUP.currentGroupEnd) {
+                waitInfo = attraction.queue.BOARDING_GROUP.currentGroupStart + '-' + attraction.queue.BOARDING_GROUP.currentGroupEnd;
+            } else {
+                return displayIconInChip(<CloseIcon/>, 'Closed');
+            }
         }
-        return waitInfo;
+        if (waitInfo === 'OPERATING') {
+            return displayIconInChip(<EventAvailableIcon/>, 'Open', 'primary');
+        }
+        return <Chip color={'primary'} label={waitInfo}/>;
     }
 
 
     function displayAttraction(attraction) {
         return (
             <TableRow key={attraction.id} sx={{'&:last-child td, &:last-child th': {border: 0}}}>
-                <TableCell align={'left'}>{attraction.name}</TableCell>
-                <TableCell align={'center'}><Chip label={displayWaitInfo(attraction)}/></TableCell>
+                <TableCell align={'left'}><h3>{attraction.name}</h3></TableCell>
+                <TableCell align={'center'}>{displayWaitInfo(attraction)}</TableCell>
             </TableRow>
         );
     }
